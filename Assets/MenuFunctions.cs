7d1d0e925558f0eda2f838a69144329fd64bce0c
FairyMental <radu-adrian.marcu@students.plymouth.ac.uk>
2017-02-19 18:39:06 +0000

    Added Skybox, "Start Journey Button" , "Quit" button and interactions for them.

---
+++ Assets/MenuFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void OnStartJourneyButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
