5aa35a794fdf0a86de98f9f710d0b1726c8afa1c
FairyMental <radu-adrian.marcu@students.plymouth.ac.uk>
2017-02-19 23:38:12 +0000

    Added small visual effect for buttons.

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuFunctions : MonoBehaviour {
 
+    [SerializeField]
+    private float speed =0.05f, maxSize = 1.3f, minSize = 1.0f, incrementBy = 0.02f,decrementBy = 0.04f;
+    [SerializeField]
+    RectTransform panel;
+
+    private float crtScale = 1.0f;
+    
 	// Use this for initialization
 	void Start () {
 		
@@ -17,5 +25,48 @@ public class MenuFunctions : MonoBehaviour {
     public void OnQuitButton()
     {
         Application.Quit();
+    }
+    public void OnHoverButton()
+    {
+        InvokeRepeating("IncrementScale", speed,speed);
+        CancelInvoke("DecrementScale");
+        print("Incrementing");
+    }
+    public void OnHoverExitButton()
+    {
+        CancelInvoke("DecrementScale");
+        CancelInvoke("IncrementScale");
+        panel.localScale = Vector3.one;
+        print("Decrementing");
+    }
+
+    private void IncrementScale()
+    {
+        panel.localScale = new Vector3(crtScale, crtScale, crtScale);
+        if (crtScale < maxSize)
+        {
+            crtScale += incrementBy;
+            print(crtScale);
+        }
+        else
+        {
+            crtScale = maxSize;
+            CancelInvoke("IncrementScale");
+            InvokeRepeating("DecrementScale",speed,speed);
+        }
+    }
+    private void DecrementScale()
+    {
+        panel.localScale = new Vector3(crtScale, crtScale, crtScale);
+        if (crtScale > minSize)
+        {
+            crtScale -= decrementBy;
+        }
+        else
+        {
+            crtScale = minSize;
+            CancelInvoke("DecrementScale");
+            InvokeRepeating("IncrementScale", speed, speed);
+        }
     }
 }
