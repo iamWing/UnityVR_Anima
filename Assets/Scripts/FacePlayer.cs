51d14df1e75213bdf53eb1e0a738525de003adad
FairyMental <radu-adrian.marcu@students.plymouth.ac.uk>
2017-02-19 19:37:55 +0000

    Added some trees to the scene, a skybox, a ground floor and all trees face the player.

---
+++ Assets/Scripts/FacePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.LookAt(GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>());
+	}
+	
+
+}
