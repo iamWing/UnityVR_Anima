cef1d943f866d02b6a6bc34148a5b12cf6c538b0
Wing Chau <ccw.wingc@gmail.com>
2017-02-21 07:02:35 +0000

    namespace utilities create custom namespace "Utilities" for general scripts across objects implement interface "IGvrPointerHoverEvent" extends "IEventSystemHandler" for main camera

---
+++ Assets/Scripts/Utilites/IGvrPointHoverEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Utilities {
+
+    internal interface IGvrPointerHoverEvent : IEventSystemHandler {
+
+        void OnGvrPointerHover(GameObject progressBar);
+    }
+}
