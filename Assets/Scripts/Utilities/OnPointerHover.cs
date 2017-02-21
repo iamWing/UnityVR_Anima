1fc2b8e86a469652a93be038816c8b0cdf103bec
Wing Chau <ccw.wingc@gmail.com>
2017-02-21 08:33:26 +0000

    implement progress bar script this script is for test only

---
+++ Assets/Scripts/Utilities/OnPointerHover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnPointerHover : MonoBehaviour {
+
+    private float hoverTime = 3.0f;
+    private Image progressBar;
+
+	// Use this for initialization
+	void Start () {
+        progressBar.fillAmount = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        if (progressBar.fillAmount < 1) {
+            progressBar.fillAmount += 1.0f / hoverTime * Time.deltaTime;
+        }
+	}
+}
