2ee19a71a8c109661c5d7763dec8e69bc1afed0b
Wing Chau <wing@devtography.com>
2017-02-27 14:26:17 +0000

    refactor namespace Utilities -> PorjectUtilities avoid namespace to be included in C# built-in Utilities

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using Utilities;
+using ProjectUtilities;
 
 public class MenuFunctions : GazeHoverBehaviour {
 
--- Assets/Scripts/Utilities/GazeHoverBehaviour.cs
+++ Assets/Scripts/Utilities/GazeHoverBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-namespace Utilities {
+namespace ProjectUtilities {
     public abstract class GazeHoverBehaviour : MonoBehaviour, IGvrPointerHoverTarget {
 
         [SerializeField]
