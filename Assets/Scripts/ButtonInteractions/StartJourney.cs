c3e671170e7d86d27c4a948be67d708cc6a6d606
Wing Chau <wing@devtography.com>
2017-02-27 14:52:29 +0000

    implement StartJourney execute action SceneManager.LoadScene(1)

---
+++ Assets/Scripts/ButtonInteractions/StartJourney.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using ButtonInteractions;
+
+public class StartJourney : StartQuitUIEffect {
+
+	public override void Execute() {
+		SceneManager.LoadScene(1);
+	}
+
+}
