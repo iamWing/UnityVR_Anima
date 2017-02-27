0c0f542ddf56bdae1dcae6587f35630c554b1370
Wing Chau <wing@devtography.com>
2017-02-27 09:39:04 +0000

    implement GazeHoverBehaviour each game object that interact with the gvr pointer should extend this class as it has defined a method pattern to execute events

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using Utilities;
 
-public class MenuFunctions : MonoBehaviour, IGvrPointerHoverTarget {
+public class MenuFunctions : GazeHoverBehaviour {
 
     [SerializeField]
     private float speed , maxSize, minSize , incrementBy ,decrementBy;
@@ -22,8 +22,8 @@ public class MenuFunctions : MonoBehaviour, IGvrPointerHoverTarget {
     [SerializeField]
     Image image;
 
-    [SerializeField]
-    private GameObject m_player; // TODO implement super class to handle target object mapping
+    //[SerializeField]
+    //private GameObject m_player; // TODO implement super class to handle target object mapping
 
     private float crtScale,crtAlpha = 0.0f;
 
@@ -40,7 +40,7 @@ public class MenuFunctions : MonoBehaviour, IGvrPointerHoverTarget {
     {
         Application.Quit();
     }
-    public void OnHoverButton()
+    protected override void OnHover()
     {
         crtScale = minSize;
         imgColor = image.color;
@@ -51,22 +51,22 @@ public class MenuFunctions : MonoBehaviour, IGvrPointerHoverTarget {
         CancelInvoke("DecrementScale");
         print("Incrementing" + " " + incrementAlphaBy + " " + decrementAlphaBy);
 
-        ExecuteEvents.Execute<IGvrPointerHoverEvent>(m_player, null, (x, y) => x.OnGvrPointerHover(this));
+        // ExecuteEvents.Execute<IGvrPointerHoverEvent>(m_player, null, (x, y) => x.OnGvrPointerHover(this));
         // TODO move to super class
 
     }
-    public void OnHoverExitButton()
+    protected override void OnHoverExit()
     {
         CancelInvoke("DecrementScale");
         CancelInvoke("IncrementScale");
         panel.localScale = new Vector3(minSize, minSize, minSize);
         print("Decrementing");
 
-        ExecuteEvents.Execute<IGvrPointerHoverEvent>(m_player, null, (x, y) => x.OnGvrPointerHoverExit());
+        // ExecuteEvents.Execute<IGvrPointerHoverEvent>(m_player, null, (x, y) => x.OnGvrPointerHoverExit());
         // TODO move to super class
     }
 
-    void IGvrPointerHoverTarget.Execute() {
+     public override void Execute() {
         OnStartJourneyButton();
     }
 
