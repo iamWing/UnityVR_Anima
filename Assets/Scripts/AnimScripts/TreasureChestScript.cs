8b938b55886725aacdbceacb69903acef36e7f77
FairyMental <radu-adrian.marcu@students.plymouth.ac.uk>
2017-03-12 16:29:14 +0000

    Added script to execute an animation for treasureChest when opened.

---
+++ Assets/Scripts/AnimScripts/TreasureChestScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChestScript : MonoBehaviour {
+
+    [SerializeField]
+    Sprite chestClosed, chestOpened;
+
+    [SerializeField]
+    GameObject particleSystem;
+    [SerializeField]
+    float particleSystemLifeTime = 5.0f;
+
+    private SpriteRenderer spriteRenderer;
+
+
+	public void Execute()
+    {
+        particleSystem.SetActive(true);
+        spriteRenderer.sprite = chestClosed;
+        Invoke("DisableParticleSystem", particleSystemLifeTime);
+
+    }
+    public void DisableParticleSystem()
+    {
+        particleSystem.SetActive(false);
+    }
+}
