2de0af9611ca71a30582ceefb54b439fbcb16b55
Wing Chau <wing@devtography.com>
2017-03-13 10:42:23 +0000

    implement CharactersTalkTrigger handles on gvr pointer hover events and trigger "CharactersTalking" clip

---
+++ Assets/Scripts/ButtonInteractions/CharactersTalkTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using ProjectUtilities;
+
+namespace ButtonInteractions {
+    public class CharactersTalkTrigger : GazeHoverBehaviour {
+
+        private Animator m_anim;
+        private int m_talkStateHash = Animator.StringToHash("CharactersTalking");
+
+        void Start() {
+            m_anim = GetComponentInParent<Animator>();
+        }
+
+        public override void Execute() {
+            m_anim.SetTrigger(m_talkStateHash);
+        }
+
+        protected override void OnHover() {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnHoverExit() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
