33c716ba089e327b470a6c0e7c0c51e75aa2dc6a
Wing Chau <wing@devtography.com>
2017-03-27 13:57:05 +0100

    implement sprite animation script for dragon

---
+++ Assets/Scripts/AnimScripts/NonStaticSpriteAnimScript.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * This script is similar to SpriteAnimScript but replace the sprites 
+ * in Image instead of SpriteRenderer.
+ **/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NonStaticSpriteAnimScript : MonoBehaviour {
+
+	[SerializeField] private Sprite[] m_animSprites;
+	[SerializeField] private float refreshRate = 0.5f;
+
+	private Image m_image;
+	private int currentSprite = 0;
+
+	// Use this for initialization
+	void Start () {
+		m_image = GetComponent<Image> ();
+		InvokeRepeating ("UpdateSprite", refreshRate, refreshRate);
+	}
+
+	void UpdateSprite() {
+		if (currentSprite == m_animSprites.Length)
+			currentSprite = 0;
+		m_image.sprite = m_animSprites [currentSprite];
+
+		currentSprite++;
+	}
+}
