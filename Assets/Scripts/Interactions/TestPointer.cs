e15113c85aadbf46e6d8d5dd752bb5403f729c55
Wing Chau <wing@devtography.com>
2017-05-13 12:59:22 +0100

    script for raycast test

---
+++ Assets/Scripts/Interactions/TestPointer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GvrUtilities;
+
+namespace Interactions {
+	public class TestPointer : GazeHoverBehaviour {
+
+		// Use this for initialization
+		void Start() {
+		
+		}
+
+		public override void Execute() {
+			
+		}
+
+		protected override void OnHover() {
+			
+		}
+
+		protected override void OnHoverExit() {
+			
+		}
+	
+	}
+}
