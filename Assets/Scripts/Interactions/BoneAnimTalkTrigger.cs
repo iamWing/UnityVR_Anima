81ae3a0616f361fd767861a25823547ce7bc815c
Wing Chau <wing@devtography.com>
2017-05-14 06:34:46 +0100

    implement walk animation Flow: Joylne found the chest -> Replace Saren object and walk to the chest Script for bone animation applied

--- Assets/Scripts/Interactions/BoneAnimTalkTrigger.cs
+++ Assets/Scripts/Interactions/BoneAnimTalkTrigger.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GvrUtilities;
+using UnityEngine.EventSystems;
 
-public class BoneAnimTalkTrigger : MonoBehaviour {
+namespace Interactions {
+	public class BoneAnimTalkTrigger : GazeHoverBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+		[SerializeField] private GameObject m_chest;
+
+		private Animator m_anim;
+		private int m_walkStateHase = Animator.StringToHash ("Move2Chest");
+
+		private bool m_executed = false;
+
+		// Use this for initialization
+		void Start () {
+			m_anim = GetComponent<Animator> ();
+		}
+
+		public override void Execute() {
+			if (!m_executed) {
+				m_anim.SetTrigger (m_walkStateHase);
+
+				m_executed = true;
+			}
+		}
+
+		protected override void OnHover() {
+			
+		}
+
+		protected override void OnHoverExit() {
+			
+		}
+
+		void ActiveChest() {
+			Debug.Log ("Active Chest");
+			ExecuteEvents.Execute<Chest>(m_chest, null, (x, y) => x.ActiveChest());
+		}
 	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
